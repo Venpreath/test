36066f8cfbb599ecb31c7a783add74861b3deb6e
Venpreath <venpreath@yandex.ru>
2022-03-02 21:23:50 +0300

    222

--- t.rb
+++ t.rb
@@ -1,14 +1,8 @@
 
 #sending to the 2nd_new_branch
 
-<<<<<<< HEAD
-#3nd_changes_in_3d_branch
-=======
->>>>>>> parent of dd18ed1... 2nd changes in 3d branch
-
-sdfds
-sdfdsdf
-sdfdss
-f
-sdfdss
-df
+asas
+asass
+asasa
+s
+asass
