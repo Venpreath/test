c95f00f226abdfedc86ccd2954cfecdfe6b09c12
Venpreath <venpreath@yandex.ru>
2022-03-02 21:24:39 +0300

    2222

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Push 3d_new_branch
 .
 #New changes in 3d_new_branch straight from 2ng_new_..
+SDSDSS
+S
+D
+SD
+S
+SD
+DS
--- t.rb
+++ t.rb
@@ -1,8 +1,9 @@
 
 #sending to the 2nd_new_branch
 
-<<<<<<< HEAD
 #3nd_changes_in_3d_branch
-=======
->>>>>>> parent of dd18ed1... 2nd changes in 3d branch
 
+sdsss
+d
+sdsssd
+sdssssd
